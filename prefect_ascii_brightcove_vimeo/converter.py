187df27ecfd72f364ac57c15883b124385bc7929
myohei <yohei.maeda.11@gmail.com>
2022-05-04 00:36:27 +0900

    fix

--- prefect_ascii_brightcove_vimeo/converter.py
+++ prefect_ascii_brightcove_vimeo/converter.py
@@ -21,8 +21,8 @@ class Converter:
         s3 = S3(aws_access_key_id=self._credentials.aws_access_key_id,
                 aws_secret_access_key=self._credentials.aws_secret_access_key,
                 region_name=self._credentials.region_name)
-        save_to_s3 = CloudManager().add(s3, bucket_name=self._bucket, )
+        save_to_s3 = CloudManager(dest_path).add(s3, bucket_name=self._bucket, )
         video = input(src_url)
         stream = video.stream2file(Formats.h264())
-        stream.output(dest_path, clouds=save_to_s3, run_command=False)
+        stream.output(clouds=save_to_s3, run_command=False)
         await stream.async_run(ffmpeg_bin='ffmpeg', monitor=monitor)
